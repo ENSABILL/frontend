46c97f6e589c15d33944731286ac71300ebc6682
Medelouali <Mohammedelouali@gmail.com>
2023-05-05 21:29:03 +0100

    Adding Ghada work

--- src/app/agent/agent.component.ts
+++ src/app/agent/agent.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+class User{
+  public nom: string='';
+  public prenom: string='';
+  public dateNaissance: string='';
+  public adresse:string='';
+  public email:string='';
+  public confirmEmail:string='';
+  public numTel:string='';
+  public numImmatriculation:number=0;
+  public numPIdentite:string='';
+  public pieceIdentite:string='';
+  public numPatente:string='';
+  public description:string='';
+  public controlFile:string='';
+  
+}
 
 @Component({
   selector: 'app-agent',
@@ -6,5 +24,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./agent.component.css']
 })
 export class AgentComponent {
+  public user: User= new User();
 
+  constructor(){};
+
+  onSubmit(registerForm: NgForm) {
+    console.log(registerForm.form);
+    console.log('valeur:', JSON.stringify(registerForm.value));
+  }
+
+  annuler() {
+    console.log('Annulation effectuée');
+    // Ajoutez ici le code pour effectuer l'annulation
+  }
 }
--- src/app/agent/agent.module.ts
+++ src/app/agent/agent.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { AgentRoutingModule } from './agent-routing.module';
 import { AgentComponent } from './agent.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { AgentComponent } from './agent.component';
   ],
   imports: [
     CommonModule,
-    AgentRoutingModule
+    AgentRoutingModule,
+    FormsModule
   ]
 })
 export class AgentModule { }
